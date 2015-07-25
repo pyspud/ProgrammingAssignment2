b4ff2f712bbeeebbecd0cbca32bddf36d20de392
Kevin Henderson <pyspud@gmail.com>
2015-07-25 12:48:10 +0100

    Assignment 2 submission
    Add my implementation of the makeCacheMatrix and cacheSolve functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,61 @@
-## Put comments here that give an overall description of what your
-## functions do
+## As calculating the Inverse can be an expensive operation the two following
+## functions can be used to cache the result, so does not need to be repeatedly
+## calcuated for the same Matrix.
 
-## Write a short comment describing this function
+## The makeCacheMatrix function creates an object that can be used to cache
+## the inverse Matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  # Cache the Matrix and clear any cached Inverse Matrix
+  set <- function(x) {
+    a <<- x
+    b <<- NULL
+  }
+  
+  # Return the Cached Matrix
+  get <- function(x) {
+    a
+  }
+  
+  # Cached the Inverse Matrix
+  setInverse <- function(x) {
+    b <<- x
+  }
+  
+  # Return the Cached Inverse Matrix
+  getInverse <- function() {
+    b
+  }
+  
+  list(set = set, 
+       get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## The cacheSolve fuction uses the object created by makeCacheMatrix function
+## to calculate and cache the result of solve function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  
+  # Get the cached Matrix Inverse
+  reciprocal <- x$getInverse()
+  
+  # If it has been calculated return it
+  if(!is.null(reciprocal)) {
+    message ("Return cached Inverse Matrix")
+    return(reciprocal)
+  }
+  
+  # Get the cached Matrix
+  a <- x$get()
+  
+  # Find the Inverse
+  reciprocal <- solve(a)
+  
+  # Cache the Inverse and return the value
+  x$setInverse(reciprocal)
+  reciprocal
 }
